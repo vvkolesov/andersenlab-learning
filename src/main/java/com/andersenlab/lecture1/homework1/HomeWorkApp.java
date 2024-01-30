915554bd796ae9ba77683606b0fe34f421e87ec7
v.v.kolesov <v.v.kolessov@gmail.com>
2024-01-30 16:50:12 +0100

    fix homework1

--- src/main/java/com/andersenlab/lecture1/homework1/HomeWorkApp.java
+++ src/main/java/com/andersenlab/lecture1/homework1/HomeWorkApp.java
@@ -5,7 +5,7 @@ public class HomeWorkApp {
     public static void main(String[] args) {
 
         printThreeWords();
-        checkSumSign(-5,-4);
+        checkSumSign(-5, -4);
         printColor(-101);
         compareNumbers(10, 10);
     }
@@ -15,17 +15,20 @@ public class HomeWorkApp {
     }
 
     public static void checkSumSign(int a, int b) {
-        if (a+b >= 0) System.out.println("Сумма положительная");
+
+        if (a + b >= 0) System.out.println("Сумма положительная");
         else System.out.println("Сумма отрицательная");
     }
 
     public static void printColor(int value) {
+
         if (value <= 0) System.out.println("Красный");
         else if (value > 100) System.out.println("Зеленый");
         else System.out.println("Желтый");
     }
 
     public static void compareNumbers(int a, int b) {
+
         if (a >= b) System.out.println("a >= b");
         else System.out.println("a < b");
     }
