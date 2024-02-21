fe3b30308663c690fba3db3edd2354903581bd2f
v.v.kolesov <v.v.kolessov@gmail.com>
2024-02-21 18:58:03 +0100

    add homework 7

--- src/main/java/com/andersenlab/lecture8/homework8/telephonebook/helpers/AddContact.java
+++ src/main/java/com/andersenlab/lecture8/homework8/telephonebook/helpers/AddContact.java
@@ -3,14 +3,14 @@ package com.andersenlab.lecture8.homework8.telephonebook.helpers;
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.andersenlab.lecture8.homework8.telephonebook.helpers.ConsoleReader.*;
+
 
 public class AddContact {
 
     public static Map<Integer, String> addContact() {
 
         Map<Integer, String> phoneBook = new HashMap<>();
-        phoneBook.put(readPhoneFromConsole(), readNameFromConsole());
+        phoneBook.put(7654, "Sidorov");
 
         return phoneBook;
     }
